d4e2ebf8ffdc48e86cfae357c6727403ef08d171
Mohammad Hammoud <mohammad.hammoud.lb@hotmail.com>
2026-06-23 01:07:28 +0300

    chore: horus v0.5.0

--- Formula/horus.rb
+++ Formula/horus.rb
@@ -1,30 +1,30 @@
 class Horus < Formula
   desc "Local-first, source-aware incident investigation engine"
   homepage "https://horus.sh"
-  version "0.4.1"
+  version "0.1.2"
   license "MIT"
 
   depends_on "node"
 
   on_macos do
     on_arm do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.4.1/horus-v0.4.1-darwin-arm64.tar.gz"
-      sha256 "fd1c63bf7643cb13d2b426f93610fba57843d48e0ab34cfb03a8e935594bcb9f"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-darwin-arm64.tar.gz"
+      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
     end
     on_intel do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.4.1/horus-v0.4.1-darwin-x86_64.tar.gz"
-      sha256 "fd1c63bf7643cb13d2b426f93610fba57843d48e0ab34cfb03a8e935594bcb9f"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-darwin-x86_64.tar.gz"
+      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.4.1/horus-v0.4.1-linux-arm64.tar.gz"
-      sha256 "fd1c63bf7643cb13d2b426f93610fba57843d48e0ab34cfb03a8e935594bcb9f"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-linux-arm64.tar.gz"
+      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
     end
     on_intel do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.4.1/horus-v0.4.1-linux-x86_64.tar.gz"
-      sha256 "fd1c63bf7643cb13d2b426f93610fba57843d48e0ab34cfb03a8e935594bcb9f"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-linux-x86_64.tar.gz"
+      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
     end
   end
 
