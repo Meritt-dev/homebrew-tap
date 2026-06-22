82f5de8bfca27ddd7d8bb1908e294d49d35491fb
Mohammad Hammoud <mohammad.hammoud.lb@hotmail.com>
2026-06-23 01:08:46 +0300

    fix: horus v0.5.0 formula (correct archives/shas)

--- Formula/horus.rb
+++ Formula/horus.rb
@@ -1,30 +1,30 @@
 class Horus < Formula
   desc "Local-first, source-aware incident investigation engine"
   homepage "https://horus.sh"
-  version "0.1.2"
+  version "0.5.0"
   license "MIT"
 
   depends_on "node"
 
   on_macos do
     on_arm do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-darwin-arm64.tar.gz"
-      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.5.0/horus-v0.5.0-darwin-arm64.tar.gz"
+      sha256 "6120a275cb0d37b8da72a9e1e3b9bb0f32da023f20411e45ecd0f7252a9924c2"
     end
     on_intel do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-darwin-x86_64.tar.gz"
-      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.5.0/horus-v0.5.0-darwin-x86_64.tar.gz"
+      sha256 "6120a275cb0d37b8da72a9e1e3b9bb0f32da023f20411e45ecd0f7252a9924c2"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-linux-arm64.tar.gz"
-      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.5.0/horus-v0.5.0-linux-arm64.tar.gz"
+      sha256 "6120a275cb0d37b8da72a9e1e3b9bb0f32da023f20411e45ecd0f7252a9924c2"
     end
     on_intel do
-      url "https://github.com/meritt-dev/horus/releases/download/v0.1.2/horus-v0.1.2-linux-x86_64.tar.gz"
-      sha256 "e92be590cd40c9ccda4ad54c36e2e2e9bfabb3fe551d2f5db03a976a48171685"
+      url "https://github.com/meritt-dev/horus/releases/download/v0.5.0/horus-v0.5.0-linux-x86_64.tar.gz"
+      sha256 "6120a275cb0d37b8da72a9e1e3b9bb0f32da023f20411e45ecd0f7252a9924c2"
     end
   end
 
